729e93cf476363799d1a6ecedaf85201acc036e5
davemins <davemins4125@naver.com>
2025-06-29 09:59:55 -0600

    feat: add initial user interface for Bartender App

--- PracticeApp1/Program.cs
+++ PracticeApp1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PRACTICE_CSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable
+            string title = "Bartender App v2.0";
+            string drinkName = "Cuba Libre";
+            //Init
+            System.Console.Title = title;
+            //Output
+            Console.WriteLine("Welcome to the Bartender App");
+            //Input
+            Console.ReadKey();
+        }
+    }
+}
--- PracticeApp1/obj/Debug/net9.0/PracticeApp1.AssemblyInfo.cs
+++ PracticeApp1/obj/Debug/net9.0/PracticeApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticeApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b515491c4d048dd8ccff79a31a8b96f73d803df")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticeApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticeApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
